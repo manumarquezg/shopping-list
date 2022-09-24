a5cae89c60ca176ea7e8373be6587668b45dc376
manumarquezg <manuel.marquez@nomasystems.com>
2022-09-24 18:44:51 +0200

    feat: fetch product list from backend and display it on frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useState, useEffect } from 'react';
+
 function App() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+      fetch('http://localhost:8000/api/v1/shoppinglist/products/')
+        .then((response) => response.json())
+        .then((data) => {
+          setProducts(data);
+        })
+        .catch((err) => {
+            console.log(err.message);
+        });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ProductsContainer">
+      <h2>Shopping list:</h2>
+      <ul>
+        {products.map((product) => {
+          return (
+              <li className="product-card" key={product.id}>
+                <p>{product.name} ({product.quantity})</p>
+              </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
