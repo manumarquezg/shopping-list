f462998ae74a4309aa88e62e51a2473181817cfa
manumarquezg <manuel.marquez@nomasystems.com>
2022-09-24 19:05:31 +0200

    feat: add form to add new products

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import axios from 'axios';
 
 function App() {
   const [products, setProducts] = useState([])
+  const [fetchProducts, setFetchProducts] = useState(true)
 
   useEffect(() => {
+    if (fetchProducts) {
       axios({
         method: 'get',
         url: 'http://localhost:8000/api/v1/shoppinglist/products/'
@@ -17,21 +19,61 @@ function App() {
         })
         .catch((err) => {
             console.log(err.message);
-        });
-  }, []);
+        })
+        .finally(() => { setFetchProducts(false)} );
+      }
+  }, [fetchProducts]);
+
+  const createProduct = event => {
+    event.preventDefault();
+    const data = {
+      name: event.target.name.value,
+      quantity: event.target.quantity.value,
+    }
+
+    axios({
+      method: 'post',
+      url: 'http://localhost:8000/api/v1/shoppinglist/products/',
+      data: data
+    })
+      .then(() => {
+        setFetchProducts(true);
+      })
+      .catch((err) => {
+          console.log(err.message);
+      });
+  }
 
   return (
-    <div className="ProductsContainer">
-      <h2>Shopping list:</h2>
-      <ul>
-        {products.map((product) => {
-          return (
-              <li className="product-card" key={product.id}>
-                <p>{product.name} ({product.quantity})</p>
-              </li>
-          );
-        })}
-      </ul>
+    <div className="ProductsPage">
+      <div className="ProductsForm">
+        <h2>Add a product to the list:</h2>
+        <form onSubmit={createProduct}>
+          <fieldset>
+            <label>
+              <p>Name</p>
+              <input name="name" />
+            </label>
+            <label>
+              <p>Quantity</p>
+              <input name="quantity" />
+            </label>
+          </fieldset>
+          <button type="submit">Add product</button>
+        </form>
+      </div>
+      <div className="ProductsList">
+        <h2>Shopping list:</h2>
+        <ul>
+          {products.map((product) => {
+            return (
+                <li className="Product" key={product.id}>
+                  <p>{product.name} ({product.quantity})</p>
+                </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
